adf4a90fb5cc8e1bcf4428b77ae966b23a78509a
Uri Barenholz <uri.barenholz@gmail.com>
2011-07-11 16:42:29 +0300

    added more plate descriptions to Converter

--- Converter.hs
+++ Converter.hs
@@ -72,8 +72,10 @@ showNewMes nm = [
 oldToNew :: (String,[Well]) -> [((PlateId, Well),String)]
 oldToNew (label,wells) = zip (zip (repeat 0) wells) . repeat $ label
 
+mapOldToNew = M.fromList  . concatMap oldToNew 
+
 bestOfBest2Desc :: Description
-bestOfBest2Desc = M.fromList  . concatMap oldToNew $ [
+bestOfBest2Desc = mapOldToNew [
 		("mA",[Well 'a' 11, Well 'a' 12]),
 		("mB",[Well 'b' 11, Well 'b' 12]),
 		("mC",[Well 'c' 11, Well 'c' 12]),
@@ -95,7 +97,7 @@ bestOfBest2Desc = M.fromList  . concatMap oldToNew $ [
 		("Unmapped", (concatMap (zipWith Well ['a'..'h'] . repeat) [7..10] \\ [Well 'a' 10]) ++ [Well 'g' 11, Well 'a' 2,Well 'a' 5,Well 'a' 6,Well 'b' 1,Well 'b' 6,Well 'c' 1,Well 'c' 2,Well 'c' 3,Well 'c' 4,Well 'c' 5,Well 'd' 2,Well 'd' 6,Well 'e' 3,Well 'e' 5,Well 'f' 2,Well 'f' 3,Well 'g' 1,Well 'g' 2,Well 'g' 4,Well 'h' 3,Well 'h' 5,Well 'h' 6])]
 
 pairsC :: Description
-pairsC = M.fromList . concatMap oldToNew $ [
+pairsC = mapOldToNew [
     ("AC", zipWith Well ['a'..'g'] . repeat $ 1),
     -- ("A2", zipWith Well ['a'..'g'] . repeat $ 2),
     ("BC", zipWith Well ['a'..'g'] . repeat $ 3),
@@ -119,7 +121,7 @@ pairsC = M.fromList . concatMap oldToNew $ [
     ("Just Media", [Well 'g' 12])]
 
 pairsD :: Description
-pairsD = M.fromList . concatMap oldToNew $ [
+pairsD = mapOldToNew [
     ("AD", zipWith Well ['a'..'g'] . repeat $ 1),
     -- ("A2", zipWith Well ['a'..'g'] . repeat $ 2),
     ("BD", zipWith Well ['a'..'g'] . repeat $ 3),
@@ -141,6 +143,49 @@ pairsD = M.fromList . concatMap oldToNew $ [
     ("Wild Type", zipWith Well ['e','f'] . repeat $ 12),
     ("Just Media", [Well 'g' 12])]
  
+plateRGBNewSingleDH5a1 = mapOldToNew [
+		("mch-L1-2m", rowWells 'a' ++ rowWells 'b'),
+		("mch-L1-ptac", rowWells 'c' ++ rowWells 'd'),
+		("mch-L2-ptac", rowWells 'e' ++ rowWells 'f'),
+		("yfp-L1-ptac", rowWells 'g' ++ rowWells 'h')
+	    ]
+
+plateRGBNewSingleDH5a2 = mapOldToNew [
+		("yfp-L2-ptac", rowWells 'a' ++ rowWells 'b'),
+		("yfp-L1-m2", rowWells 'c'),
+--		("cfp-L1-ptac", rowWells 'd'),
+--		("cfp-L2-ptac", rowWells 'e'),
+--		("cfp-L1-m2", rowWells 'f'),
+		("control YFP a-z", wPartialRow 'g' (1,6)),
+		("control mCherry a-z", wPartialRow 'g' (7,12)),
+--		("control CFP a-z", wPartialRow 'h' (1,6)),
+		("control wt", wPartialRow 'h' (7,9))
+			    ]
+
+plateRGBNewSingleK121 = mapOldToNew [
+		("mch-L1-2m", rowWells 'a' ++ rowWells 'h'),
+		("mch-L1-ptac", rowWells 'b' ++ rowWells 'c'),
+		("mch-L2-ptac", rowWells 'd' ++ rowWells 'e'),
+		("yfp-L1-2m", rowWells 'f' ++ rowWells 'g')
+	    ]
+plateRGBNewSingleK122 =  mapOldToNew [
+		("yfp-L1-ptac", rowWells 'a' ++ rowWells 'b'),
+		("yfp-L2-ptac", rowWells 'c'),
+--		("cfp-L1-ptac", rowWells 'd'),
+--		("cfp-L2-ptac", rowWells 'e'),
+--		("cfp-L1-m2", rowWells 'f'),
+		("control YFP a-z", wPartialRow 'g' (1,6)),
+		("control mCherry a-z", wPartialRow 'g' (7,12)),
+--		("control CFP a-z", wPartialRow 'h' (1,6)),
+		("control wt", wPartialRow 'h' (7,9))
+			    ]
+-- Plate utils...
+
+wPartialRow :: Char -> (Int,Int) -> [Well]
+wPartialRow row (a,b)= zipWith Well (repeat row) [a..b]
+
+rowWells :: Char -> [Well]
+rowWells row = wPartialRow row (1,12)
 convertMes :: Description -> OldMeasurement -> Maybe NewMeasurment
 convertMes d om = do
     ml <- M.lookup (0,mWell om) d
