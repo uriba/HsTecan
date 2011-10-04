d4a3402ec611976f9a153b4fe1f5d300d6ce730f
Uri Barenholz <uri.barenholz@gmail.com>
2011-10-04 16:06:49 +0200

    updated cabal file to reflect RoboSite dependencies

--- RoboAlg.hs
+++ RoboAlg.hs
@@ -1,4 +1,4 @@
-module RoboAlg (linFitWindow, expFitWindow, maxGrowth, doublingTime, expLevel)
+module RoboAlg (linFitWindow, expFitWindow, maxGrowth, doublingTime, expLevel, expLevelsN)
 where
 import Data.Function (on)
 import Data.List (sortBy)
@@ -22,8 +22,11 @@ expLevel ods ms = maximum (1 : (catMaybes $ zipWith exp_level ms_fit ods_fit))
         ms_fit = expFitWindow twoHoursWindow . toPoints $ ms
         exp_level f1 f2 = if fdRSqr f1 < 0.9 || fdRSqr f2 < 0.9 then Nothing else Just (fdBeta f1 - fdBeta f2)
 
--- expLevels :: [(Double,Seconds)] -> [(Double,Seconds)]-> [(Double,Seconds)]
--- expLevels = 
+expLevelsN :: [(Double,Seconds)] -> [(Double,Seconds)]-> [Double]
+expLevelsN ods ms = zipWith ((-) `on` fdBeta) ms_fit ods_fit
+    where
+        ods_fit = expFitWindow twoHoursWindow . toPoints $ ods
+        ms_fit = expFitWindow twoHoursWindow . toPoints $ ms
 
 doublingTime :: [(Double, Seconds)] -> Double
 doublingTime = (/) 1 . maxGrowth
