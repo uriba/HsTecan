38eb1623d63d89695b3dbc5cc4d8ae1c8340fc92
Uri Barenholz <uri.barenholz@gmail.com>
2011-10-24 17:39:15 +0200

    updated cabal file, saved some code in Smoothing that should be refactored elsewhere in the future

--- Biolab/Smoothing.hs
+++ Biolab/Smoothing.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Biolab.Smoothing (
     smoothAll,
     bFiltS,
@@ -9,6 +10,11 @@ import Data.Function (on)
 import Data.Maybe (mapMaybe)
 import RoboUtils (mean)
 import qualified Data.Map as M
+import qualified Data.ListLike as LL
+import Data.ListLike.Vector.Unboxed
+import qualified Statistics.Sample as S
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector as V
 
 windowSize = 5
 
@@ -20,7 +26,7 @@ changeVal m x = m {mVal = x}
 smoothMes :: SmoothDataset -> [Measurement] -> [Measurement]
 smoothMes f ms = zipWith ($) (map changeVal ordered_data) . f . map mVal $ ordered_data
     where
-	ordered_data = sortBy (compare `on` mTime) ms
+        ordered_data = sortBy (compare `on` mTime) ms
 
 applySmooth' :: SmoothDataset -> MType -> [Measurement] -> [Measurement]
 applySmooth' f mt ms = not_affected ++ smoothed
@@ -37,10 +43,10 @@ smoothAll f ed = foldr (applySmooth f) ed ["OD600","MCHERRY","YFP","CFP"]
 bFiltS :: SmoothDataset
 bFiltS ds = mapMaybe (bFilt . take windowSize) . tails $ extended_dataset
     where
-	extended_dataset = header ++ ds ++ trailer
-	header = replicate buf_size . head $ ds
-	trailer = replicate buf_size . last $ ds
-	buf_size = windowSize `div` 2
+        extended_dataset = header ++ ds ++ trailer
+        header = replicate buf_size . head $ ds
+        trailer = replicate buf_size . last $ ds
+        buf_size = windowSize `div` 2
 
 bFilt :: [Double] -> Maybe Double
 bFilt xs
@@ -53,3 +59,23 @@ bFilt xs
 	    val = Just . mean 
 	    relevant_data = filter (\x -> x < mx && x > mn) $ xs
 
+smoothMean :: Maybe Int -> S.Sample -> S.Sample
+smoothMean Nothing = smoothMean . Just $ windowSize
+smoothMean span = smooth S.mean span
+
+smooth :: (G.Vector v a)  => (v a -> a) -> Maybe Int -> v a -> v a
+smooth f Nothing d = smooth f (Just windowSize) d
+smooth f (Just span) d = G.fromList . map (f) . slidingWindow span $ d
+
+vTails :: (G.Vector v a) => v a -> [v a]
+vTails v
+    | G.null v = [G.empty]
+    | otherwise = v : vTails (G.tail v)
+
+slidingWindow :: (G.Vector v a) =>  Int -> v a -> [v a]
+slidingWindow span d = map (G.take span) . take (G.length d) . vTails $ expanded_dataset
+    where
+        expanded_dataset = header G.++ d G.++ trailer
+        header = G.replicate buf_size . G.head $ d
+        trailer = G.replicate buf_size . G.last $ d
+        buf_size = span `div` 2
