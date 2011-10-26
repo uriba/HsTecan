d01bc3fdee9d478a639c6cf280d50263c5cbfdc2
Uri Barenholz <uri.barenholz@gmail.com>
2011-10-26 10:55:23 +0200

    updated constants for better identification of live colonies

--- Biolab/Constants.hs
+++ Biolab/Constants.hs
@@ -22,7 +22,7 @@ mediaId = "BLANK"  -- label of colonies that have no bacteria (for background ca
 minValMap :: MesTypeCorrectionVals
 minValMap = fromList [("OD600",0.005), ("YFP", 20), ("MCHERRY",10),("CFP",10), ("GFP",10)]
 
-odLiveThreshold = 0.2
+odLiveThreshold = 0.1
 odThreshold = 0.005
 stdMinOd = 0.04
 
